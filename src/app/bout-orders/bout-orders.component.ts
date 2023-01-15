0c673af7f1aca3b24f4c5118aa9e22472b35d401
Michael Buckley <michaelabuckley@gmail.com>
2023-01-15 18:14:28 -0500

    Add bout orders

--- src/app/bout-orders/bout-orders.component.ts
+++ src/app/bout-orders/bout-orders.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import boutOrdersCsv from 'raw-loader!./bout-orders-data.csv';
 
+
+let rawRows: string[][] = boutOrdersCsv.split("\n").map(row=>row.split(',').map(s=>s.trim()));
+let headers = rawRows[0];
+let data = rawRows.slice(1);
+
+const orderIndex: Record<string, number> = Object.fromEntries(headers.map((s,i)=>[s,i]));
+
+const boutOrders: string[][] =
+  headers.map(n=>
+      data.map(r => r[orderIndex[n]]).filter(s=>s)
+  );
+
 @Component({
-  selector: 'app-bout-orders',
+  selector: 'bout-orders',
   templateUrl: './bout-orders.component.html',
   styleUrls: ['./bout-orders.component.less']
 })
 
-// const boutOrders = {
-//   boundOrders: boutOrders
-// }
+
 export class BoutOrdersComponent {
-  boutOrders = boutOrders
+  boutOrders: string[][] = boutOrders
+  orderIndex: any = orderIndex
+
 
 }
