affd5c75becbbc4c512ec8485f8c4e5e5ae33a7b
Michael Buckley <michaelabuckley@gmail.com>
2023-01-22 11:27:33 -0500

    Use grid layout in header and bout block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
+// wipmb margins for printing - check out print stylesheets
+// wipmb can we use flex-box to fill print size?
+// wipmb basic de blocks with food/schedule notes
+// wipmb rebuild project under ps namespace
+
 export class AppComponent {
   title = 'Pool Sheet';
 }
